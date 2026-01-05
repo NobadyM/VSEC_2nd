bb9cb2e198ab550f21cecbce88e1268ff480f15b
Mandar Wasekar <wasekarmandar@gmail.com>
2026-01-05 09:50:46 +0530

    Update Fname.cpp

--- Fname.cpp
+++ Fname.cpp
@@ -1,11 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int a = 11, b = 9;
+    int age;
 
-    // Adding the two numbers and printing
-    // their sum
-    cout << a + b;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "You are eligible (Age is 18 or above).";
+    } else {
+        cout << "You are not eligible (Below 18).";
+    }
+
     return 0;
 }
